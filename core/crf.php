53a1bd8568e4cd51430b5459f53691a06a7c9600
roywalk3r <projectmail811@gmail.com>
2025-07-16 21:01:39 +0000

    track all

---
+++ core/crf.php
@@ -0,0 +1,21 @@
+<?php
+// CSRF Protection
+session_start();
+function csrf_token() {
+    if (empty($_SESSION['csrf_token'])) {
+        $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+    }
+    return $_SESSION['csrf_token'];
+}
+function csrf_field() {
+    $token = csrf_token();
+    return '<input type="hidden" name="csrf_token" value="' . htmlspecialchars($token) . '">';
+}
+function verify_csrf() {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if (empty($_POST['csrf_token']) || !hash_equals($_SESSION['csrf_token'], $_POST['csrf_token'])) {
+            die('Invalid CSRF token');
+        }
+    }
+}
+?>
