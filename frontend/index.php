67ff99e9ba4d6e627cb376fea1438efe59889fb3
roywalk3r <projectmail811@gmail.com>
2025-07-19 23:20:16 +0000

    Started frontend dev and added assets

---
+++ frontend/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Nananom Farms is a web-based palm oil management system for farmers, buyers, and other stakeholders to manage their palm oil business efficiently.">
+    <meta name="keywords" content="palm oil, management, farmers, buyers, business, efficient">
+    <meta name="author" content="Roy Walk">
+    <meta name="copyright" content="Copyright 2022 Nananom Farms">
+    <meta name="robots" content="index, follow">
+    <meta name="googlebot" content="index, follow">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Nananom Farms | Home</title>
+</head>
+<body>
+<?php
+include_once __DIR__ . '/../frontend/partials/header.php';
+?>
+<main class="container">
+    <div class="hero">
+        <div class="overlay"></div>
+     <div class="hero-image">
+         <img src="assets/images/hero_hp_pic.jpeg/" alt="Hero Image">
+     </div>
+        <div class="hero-content">
+            <h2>Your Trusted Palm Oil Partner</h2>
+            <p>Delivering Premium palm oil and reliable farm services, experience,quality tradition, and trust with Nananom Farms.</p>
+            <a href="about.php" class="ctaBtn">Book a Service</a>
+        </div>
+
+        <div class="about-us">
+
+        </div>
+    </div>
+</main>
+</body>
+</html>
